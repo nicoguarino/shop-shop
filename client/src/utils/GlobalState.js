8ab3ad7ad477a0b4dc87efc0bd3056cdc38f82c2
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-12-20 15:32:40 -0800

    created functionality for add and remove cart, edit quanitites

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -7,13 +7,17 @@ const { Provider } = StoreContext;
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useProductReducer({
         products: [],
+        cart: [],
+        cartOpen: false,
         categories: [],
-        currentCategory: '',
+        currentCategory: ''
     });
     // use this to confirm it works!
     console.log(state);
     return <Provider value={[state, dispatch]} {...props} />;
 };
+
+
 
 const useStoreContext = () => {
     return useContext(StoreContext);
